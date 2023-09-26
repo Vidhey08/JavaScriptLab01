61f807d7cdc61c0501501a7eccbb2f81d4b34848
Vidhey08 <120338677+Vidhey08@users.noreply.github.com>
2023-09-25 23:49:15 -0400

    Update silly-story.js

--- js/silly-story.js
+++ js/silly-story.js
@@ -1,38 +1,38 @@
-// VARIABLE DECLARATIONS
+
 let customName = document.getElementById('customname');
 let randomize = document.querySelector('.randomize');
 let story = document.querySelector('.story-output');
 
 
-// Story text and placeholder values
+
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = ['Donald Trump', 'Jackie Chan', 'Santa Claus'];
 let insertY = ['Area 51', 'Death Valley', 'Aruba'];
 let insertZ = ['spontaneously combusted', 'rapidly sublimated', 'evaporated instantly'];
 
-// Function to randomly select from an array
+
 function randomValueFromArray(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-// Function to generate the story
+
 function result() {
     let newStory = storyText;
 
-    // Get random values from the arrays
+   
     let xItem = randomValueFromArray(insertX);
     let yItem = randomValueFromArray(insertY);
     let zItem = randomValueFromArray(insertZ);
 
-    // Replace placeholders in the story
+    
     newStory = newStory.replace(/:insertx:/g, xItem).replace(':inserty:', yItem).replace(':insertz:', zItem);
 
-    // Check for custom name and replace 'Bob' if a custom name is entered
+    
     if (customName.value !== '') {
         newStory = newStory.replace('Bob', customName.value);
     }
 
-    // Metric conversion
+    
     if (document.getElementById("metric").checked) {
         let weight = Math.round(300 * 0.453592) + ' kg';
         let temperature = Math.round((94 - 32) * 5/9) + '°C';
@@ -43,5 +43,5 @@ function result() {
     story.style.visibility = 'visible';
 }
 
-// Attach event listener to the button
+
 randomize.addEventListener("click", result);
